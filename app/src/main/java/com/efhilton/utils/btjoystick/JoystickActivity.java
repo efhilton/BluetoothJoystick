10621cc3022d6465142e197378608991246a5a51
Edgar Hilton <edgar.hilton@gmail.com>
2025-02-05 11:23:23 -0600

    First working version.

--- app/src/main/java/com/efhilton/utils/btjoystick/JoystickActivity.java
+++ app/src/main/java/com/efhilton/utils/btjoystick/JoystickActivity.java
@@ -44,8 +44,17 @@ public class JoystickActivity extends AppCompatActivity {
         @Override
         public void run() {
             // Delayed removal of status and navigation bar
-            mContentView.getWindowInsetsController().hide(
-                    WindowInsets.Type.statusBars() | WindowInsets.Type.navigationBars());
+            if (Build.VERSION.SDK_INT >= 30) {
+                mContentView.getWindowInsetsController().hide(
+                        WindowInsets.Type.statusBars() | WindowInsets.Type.navigationBars());
+            } else {
+                mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+            }
         }
     };
     private View mControlsView;
@@ -154,8 +163,13 @@ public class JoystickActivity extends AppCompatActivity {
 
     private void show() {
         // Show the system bar
-        mContentView.getWindowInsetsController().show(
-                WindowInsets.Type.statusBars() | WindowInsets.Type.navigationBars());
+        if (Build.VERSION.SDK_INT >= 30) {
+            mContentView.getWindowInsetsController().show(
+                    WindowInsets.Type.statusBars() | WindowInsets.Type.navigationBars());
+        } else {
+            mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
+        }
         mVisible = true;
 
         // Schedule a runnable to display UI elements after a delay
