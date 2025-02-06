d3bfc5d8972f8523d75316f0ea30d2743d4967c1
Edgar Hilton <edgar.hilton@gmail.com>
2025-02-05 22:01:23 -0600

    Fully working version without connecting to anything.  Next stage will be to add settings to correctly discover and connect to a remote Bluetooth server.

--- app/src/main/java/com/efhilton/utils/btjoystick/JoystickActivity.java
+++ app/src/main/java/com/efhilton/utils/btjoystick/JoystickActivity.java
@@ -1,104 +1,17 @@
 package com.efhilton.utils.btjoystick;
 
-import android.annotation.SuppressLint;
-
-import androidx.appcompat.app.ActionBar;
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.os.Build;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Looper;
-import android.view.MotionEvent;
 import android.view.View;
-import android.view.WindowInsets;
+import android.widget.CheckBox;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.efhilton.utils.btjoystick.databinding.ActivityJoystickBinding;
 
-/**
- * An example full-screen activity that shows and hides the system UI (i.e.
- * status bar and navigation/system bar) with user interaction.
- */
 public class JoystickActivity extends AppCompatActivity {
-    /**
-     * Whether or not the system UI should be auto-hidden after
-     * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
-     */
-    private static final boolean AUTO_HIDE = true;
+    private JoystickView thumbstickLeftStick;
+    private JoystickView thumbstickRightStick;
 
-    /**
-     * If {@link #AUTO_HIDE} is set, the number of milliseconds to wait after
-     * user interaction before hiding the system UI.
-     */
-    private static final int AUTO_HIDE_DELAY_MILLIS = 3000;
-
-    /**
-     * Some older devices needs a small delay between UI widget updates
-     * and a change of the status and navigation bar.
-     */
-    private static final int UI_ANIMATION_DELAY = 300;
-    private final Handler mHideHandler = new Handler(Looper.myLooper());
-    private View mContentView;
-    private final Runnable mHidePart2Runnable = new Runnable() {
-        @SuppressLint("InlinedApi")
-        @Override
-        public void run() {
-            // Delayed removal of status and navigation bar
-            if (Build.VERSION.SDK_INT >= 30) {
-                mContentView.getWindowInsetsController().hide(
-                        WindowInsets.Type.statusBars() | WindowInsets.Type.navigationBars());
-            } else {
-                mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
-                        | View.SYSTEM_UI_FLAG_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
-            }
-        }
-    };
-    private View mControlsView;
-    private final Runnable mShowPart2Runnable = new Runnable() {
-        @Override
-        public void run() {
-            // Delayed display of UI elements
-            ActionBar actionBar = getSupportActionBar();
-            if (actionBar != null) {
-                actionBar.show();
-            }
-            mControlsView.setVisibility(View.VISIBLE);
-        }
-    };
-    private boolean mVisible;
-    private final Runnable mHideRunnable = new Runnable() {
-        @Override
-        public void run() {
-            hide();
-        }
-    };
-    /**
-     * Touch listener to use for in-layout UI controls to delay hiding the
-     * system UI. This is to prevent the jarring behavior of controls going away
-     * while interacting with activity UI.
-     */
-    private final View.OnTouchListener mDelayHideTouchListener = new View.OnTouchListener() {
-        @Override
-        public boolean onTouch(View view, MotionEvent motionEvent) {
-            switch (motionEvent.getAction()) {
-                case MotionEvent.ACTION_DOWN:
-                    if (AUTO_HIDE) {
-                        delayedHide(AUTO_HIDE_DELAY_MILLIS);
-                    }
-                    break;
-                case MotionEvent.ACTION_UP:
-                    view.performClick();
-                    break;
-                default:
-                    break;
-            }
-            return false;
-        }
-    };
     private ActivityJoystickBinding binding;
 
     @Override
@@ -108,81 +21,111 @@ public class JoystickActivity extends AppCompatActivity {
         binding = ActivityJoystickBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        mVisible = true;
-        mControlsView = binding.fullscreenContentControls;
-        mContentView = binding.fullscreenContent;
+        View.OnClickListener switchToggled = (v) -> {
+            CheckBox switchCompat = (CheckBox) v;
+            int id = v.getId();
+            boolean isChecked = switchCompat.isChecked();
+            int nameId;
 
-        // Set up the user interaction to manually show or hide the system UI.
-        mContentView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                toggle();
+            if (id == R.id.switch00) {
+                nameId = R.string.f00;
+            } else if (id == R.id.switch01) {
+                nameId = R.string.f01;
+            } else if (id == R.id.switch02) {
+                nameId = R.string.f02;
+            } else if (id == R.id.switch03) {
+                nameId = R.string.f03;
+            } else if (id == R.id.switch04) {
+                nameId = R.string.f04;
+            } else if (id == R.id.switch05) {
+                nameId = R.string.f05;
+            } else if (id == R.id.switch06) {
+                nameId = R.string.f06;
+            } else if (id == R.id.switch07) {
+                nameId = R.string.f07;
+            } else if (id == R.id.switch08) {
+                nameId = R.string.f08;
+            } else if (id == R.id.switch09) {
+                nameId = R.string.f09;
+            } else if (id == R.id.switch10) {
+                nameId = R.string.f10;
+            } else if (id == R.id.switch11) {
+                nameId = R.string.f11;
+            } else {
+                return;
             }
-        });
 
-        // Upon interacting with UI controls, delay any scheduled hide()
-        // operations to prevent the jarring behavior of controls going away
-        // while interacting with the UI.
-        binding.dummyButton0.setOnTouchListener(mDelayHideTouchListener);
-        binding.dummyButton1.setOnTouchListener(mDelayHideTouchListener);
-        binding.dummyButton2.setOnTouchListener(mDelayHideTouchListener);
-        binding.dummyButton3.setOnTouchListener(mDelayHideTouchListener);
+            String name = getString(nameId);
+            printSwitchStatusChange(name, isChecked);
+        };
+        binding.switch00.setOnClickListener(switchToggled);
+        binding.switch01.setOnClickListener(switchToggled);
+        binding.switch02.setOnClickListener(switchToggled);
+        binding.switch03.setOnClickListener(switchToggled);
+        binding.switch04.setOnClickListener(switchToggled);
+        binding.switch05.setOnClickListener(switchToggled);
+        binding.switch06.setOnClickListener(switchToggled);
+        binding.switch07.setOnClickListener(switchToggled);
+        binding.switch08.setOnClickListener(switchToggled);
+        binding.switch09.setOnClickListener(switchToggled);
+        binding.switch10.setOnClickListener(switchToggled);
+        binding.switch11.setOnClickListener(switchToggled);
+
+        View.OnClickListener buttonPressed = v -> {
+            int nameId;
+            if (v.getId() == R.id.button00) {
+                nameId = R.string.b00;
+            } else if (v.getId() == R.id.button01) {
+                nameId = R.string.b01;
+            } else if (v.getId() == R.id.button02) {
+                nameId = R.string.b02;
+            } else if (v.getId() == R.id.button03) {
+                nameId = R.string.b03;
+            } else if (v.getId() == R.id.button04) {
+                nameId = R.string.b04;
+            } else if (v.getId() == R.id.button05) {
+                nameId = R.string.b05;
+            } else if (v.getId() == R.id.button06) {
+                nameId = R.string.b06;
+            } else if (v.getId() == R.id.button07) {
+                nameId = R.string.b07;
+            } else if (v.getId() == R.id.button08) {
+                nameId = R.string.b08;
+            } else {
+                return;
+            }
+            String name = getString(nameId);
+            System.out.println("Button " + name + " pressed");
+        };
+        binding.button00.setOnClickListener(buttonPressed);
+        binding.button01.setOnClickListener(buttonPressed);
+        binding.button02.setOnClickListener(buttonPressed);
+        binding.button03.setOnClickListener(buttonPressed);
+        binding.button04.setOnClickListener(buttonPressed);
+        binding.button05.setOnClickListener(buttonPressed);
+        binding.button06.setOnClickListener(buttonPressed);
+        binding.button07.setOnClickListener(buttonPressed);
+        binding.button08.setOnClickListener(buttonPressed);
+
+        thumbstickLeftStick = binding.thumbstickLeft;
+        thumbstickLeftStick.onMoveCallback = (x, y) -> {
+            System.out.println("LEFT Joystick: X = " + x + ", Y = " + y);
+            return null;
+        };
+        thumbstickRightStick = binding.thumbstickRight;
+        thumbstickRightStick.onMoveCallback = (x, y) -> {
+            System.out.println("RIGHT Joystick: X = " + x + ", Y = " + y);
+            return null;
+        };
+    }
+
+    private void printSwitchStatusChange(String fcnName, boolean isChecked) {
+        System.out.println("Function " + fcnName + " is " + (isChecked ? "ON" : "OFF"));
     }
 
     @Override
     protected void onPostCreate(Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
-
-        // Trigger the initial hide() shortly after the activity has been
-        // created, to briefly hint to the user that UI controls
-        // are available.
-        delayedHide(100);
     }
 
-    private void toggle() {
-        if (mVisible) {
-            hide();
-        } else {
-            show();
-        }
-    }
-
-    private void hide() {
-        // Hide UI first
-        ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null) {
-            actionBar.hide();
-        }
-        mControlsView.setVisibility(View.GONE);
-        mVisible = false;
-
-        // Schedule a runnable to remove the status and navigation bar after a delay
-        mHideHandler.removeCallbacks(mShowPart2Runnable);
-        mHideHandler.postDelayed(mHidePart2Runnable, UI_ANIMATION_DELAY);
-    }
-
-    private void show() {
-        // Show the system bar
-        if (Build.VERSION.SDK_INT >= 30) {
-            mContentView.getWindowInsetsController().show(
-                    WindowInsets.Type.statusBars() | WindowInsets.Type.navigationBars());
-        } else {
-            mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
-        }
-        mVisible = true;
-
-        // Schedule a runnable to display UI elements after a delay
-        mHideHandler.removeCallbacks(mHidePart2Runnable);
-        mHideHandler.postDelayed(mShowPart2Runnable, UI_ANIMATION_DELAY);
-    }
-
-    /**
-     * Schedules a call to hide() in delay milliseconds, canceling any
-     * previously scheduled calls.
-     */
-    private void delayedHide(int delayMillis) {
-        mHideHandler.removeCallbacks(mHideRunnable);
-        mHideHandler.postDelayed(mHideRunnable, delayMillis);
-    }
 }
---
+++ app/src/main/java/com/efhilton/utils/btjoystick/JoystickView.java
@@ -0,0 +1,150 @@
+package com.efhilton.utils.btjoystick;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Locale;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import kotlin.jvm.functions.Function2;
+
+public class JoystickView extends FrameLayout {
+
+    public Function2<Float, Float, Void> onMoveCallback;
+    private TextView xLabel;
+    private TextView yLabel;
+
+    public JoystickView(Context context) {
+        super(context);
+        init(null, 0);
+    }
+
+    public JoystickView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(attrs, 0);
+    }
+
+    public JoystickView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init(attrs, defStyle);
+    }
+
+    private void init(AttributeSet attrs, int defStyle) {
+        onMoveCallback = (x, y) -> {
+            System.out.println("Joystick: X = " + x + ", Y = " + y);
+            return null;
+        };
+
+        LayoutInflater inflater = LayoutInflater.from(getContext());
+        inflater.inflate(R.layout.joystick_view, this, true);
+
+        // Get references to the ImageViews
+        ImageView thumbStick = findViewById(R.id.thumbstick);
+        ImageView thumbStickBg = findViewById(R.id.thumbstick_bg);
+        xLabel = findViewById(R.id.x_values);
+        yLabel = findViewById(R.id.y_values);
+
+        setupJoystick(thumbStick, thumbStickBg);
+    }
+
+    private void setupJoystick(ImageView joystick, ImageView joystickBg) {
+        AtomicInteger joystickIndex = new AtomicInteger(-1);
+
+        joystick.setOnClickListener(null);
+        joystick.setOnTouchListener((view, event) -> {
+            if (view != joystick) {
+                return true;
+            }
+
+            view.getParent().requestDisallowInterceptTouchEvent(true);
+            final ImageView bg = joystickBg;
+            switch (event.getAction()) {
+                case MotionEvent.ACTION_DOWN:
+                    if (joystickIndex.get() == -1) {
+                        final int pointerIndex = event.getActionIndex();
+                        joystickIndex.set(pointerIndex);
+                    }
+                    centerThumbstick(joystick, onMoveCallback, bg);
+
+                    break;
+                case MotionEvent.ACTION_MOVE: {
+                    trackThumbMovements(joystick, onMoveCallback, event, joystickIndex.get(), bg);
+                    break;
+                }
+                case MotionEvent.ACTION_UP:
+                    centerThumbstick(joystick, onMoveCallback, bg);
+                    break;
+                case MotionEvent.ACTION_CANCEL: {
+                    centerThumbstick(joystick, onMoveCallback, bg);
+                    break;
+                }
+                default:
+                    System.out.println("Default triggered");
+                    break;
+            }
+            return true;
+        });
+    }
+
+    private void trackThumbMovements(ImageView joystick, Function2<Float, Float, Void> onMove, MotionEvent event,
+                                     int pointerIndex, ImageView bg) {
+        final float rawEventX = event.getRawX(pointerIndex);
+        final float rawEventY = event.getRawY(pointerIndex);
+
+        int[] bgLocation = new int[2];
+        bg.getLocationOnScreen(bgLocation);
+        final float bgCenterX = bgLocation[0] + bg.getWidth() / 2f; // Center on the screen
+        final float bgCenterY = bgLocation[1] + bg.getHeight() / 2f; // Center on the screen
+        final float bgRadius = bg.getWidth() / 2f;
+        final float limitRadius = bgRadius - joystick.getWidth() / 2f;
+
+        // Calculate the difference between the touch point and the center (in screen coordinates)
+        final float dx = rawEventX - bgCenterX;
+        final float dy = rawEventY - bgCenterY;
+        final float eventDistance = (float) Math.sqrt(dx * dx + dy * dy);
+        final float eventAngle = (float) Math.atan2(dy, dx);
+
+        final float limitedDistance = Math.min(eventDistance, limitRadius);
+        final float newX = bgCenterX + limitedDistance * (float) Math.cos(eventAngle); // Screen coordinates
+        final float newY = bgCenterY + limitedDistance * (float) Math.sin(eventAngle); // Screen coordinates
+
+        // Convert the new screen coordinates to view-relative coordinates for the joystick
+        int[] joystickParentLocation = new int[2];
+        ((View) joystick.getParent()).getLocationOnScreen(joystickParentLocation);
+        final float joystickNewX = newX - joystickParentLocation[0] - joystick.getWidth() / 2f;
+        final float joystickNewY = newY - joystickParentLocation[1] - joystick.getHeight() / 2f;
+
+        joystick.setX(joystickNewX);
+        joystick.setY(joystickNewY);
+
+        float normalizedX = (newX - bgCenterX) / limitRadius;
+        float normalizedY = -(newY - bgCenterY) / limitRadius;
+        xLabel.setText(String.format("X=%.2f", normalizedX));
+        yLabel.setText(String.format("Y=%.2f", normalizedY));
+        if (onMove != null) {
+            onMove.invoke(normalizedX, normalizedY);
+        }
+    }
+
+    private void centerThumbstick(ImageView joystick, Function2<Float, Float, Void> onMove, ImageView bg) {
+        float centerX = bg.getX() + bg.getWidth() / 2f;
+        float centerY = bg.getY() + bg.getHeight() / 2f;
+        joystick.animate().x(centerX - joystick.getWidth() / 2f).y(centerY - joystick.getHeight() / 2f).setDuration(100).start();
+
+        updateDisplayValues(0, 0, onMove);
+    }
+
+    private void updateDisplayValues(float x, float y, Function2<Float, Float, Void> onMove) {
+        xLabel.setText(String.format(Locale.ENGLISH, "X=%.2f", x));
+        yLabel.setText(String.format(Locale.ENGLISH, "Y=%.2f", y));
+        if (onMove != null) {
+            onMove.invoke(0f, 0f);
+        }
+    }
+}
